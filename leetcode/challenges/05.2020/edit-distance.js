2b7a0fa7ec7e5cc15c37832f1b9a63b7e07533df
Mohamed Shaaban <mohamed.shaaban@careem.com>
2020-06-01 16:36:30 +0200

    may-challenge(31): edit distance

---
+++ leetcode/challenges/05.2020/edit-distance.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function (word1, word2) {
+  const m = word1.length;
+  const n = word2.length;
+  const dp = Array(m + 1);
+
+  for (let i = 0; i <= m; i++) {
+    dp[i] = Array(n + 1);
+    for (let j = 0; j <= n; j++) {
+      if (i === 0) {
+        dp[i][j] = j;
+      } else if (j === 0) {
+        dp[i][j] = i;
+      } else if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else {
+        dp[i][j] = 1 + Math.min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]);
+      }
+    }
+  }
+
+  return dp[m][n];
+};
+
+console.log(minDistance('horse', 'ros') === 3);
+console.log(minDistance('intention', 'execution') === 5);
